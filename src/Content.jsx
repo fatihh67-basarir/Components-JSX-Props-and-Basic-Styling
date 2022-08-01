971c9534cc47be5130d0e08f4a34e9e03070d40b
fatihh67-basarir <fatih.basarir67@gmail.com>
2022-08-01 14:30:55 +0300

    style change

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,14 +1,24 @@
 
 import img2 from './img/cicekk.png';
+import "./Content.css";
 
 
 const Content = () => {
+const imgStyle = {
+    display: "block",
+    width: "350px",
+    margin: "1rem auto",
+}    
   return (
-    <div>
-        <h2>React JS</h2>
-        <p>React, Kullanici arabirimi tasarlamak için kullanılan bir JS kütüphanesidir.</p>
-        <img src="https://cdn.pixabay.com/photo/2022/05/18/12/04/flower-7205105_960_720.jpg" alt="cicek" />
-        <img src={img2} alt="img2" />
+    <div className='content'>
+        <h2 style={{color:'red'}}>React JS</h2>
+        <p style={{backgroundColor:"yellow"}}>React, Kullanici arabirimi tasarlamak için kullanılan bir JS kütüphanesidir.</p>
+        <img style={imgStyle} 
+        src="https://cdn.pixabay.com/photo/2022/05/18/12/04/flower-7205105_960_720.jpg" alt="cicek" />
+        <img 
+        style={imgStyle} 
+        src={img2} alt="img2" />
+        <p className='par-1'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima odit atque voluptate, aspernatur nemo fugit beatae iure praesentium necessitatibus nam nulla quos corrupti ducimus molestiae doloremque? Quia harum nulla ipsum laboriosam, provident explicabo doloremque eius nisi sed enim illum eum!</p>
     </div>
   )
 }
