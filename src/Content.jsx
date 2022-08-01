d30e554367ddfc7bef8c4bab1816a68ddce40d13
fatihh67-basarir <fatih.basarir67@gmail.com>
2022-08-01 14:02:20 +0300

    img change

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,8 +1,15 @@
 
+import img2 from './img/cicekk.png';
+
 
 const Content = () => {
   return (
-    <div>Content</div>
+    <div>
+        <h2>React JS</h2>
+        <p>React, Kullanici arabirimi tasarlamak için kullanılan bir JS kütüphanesidir.</p>
+        <img src="https://cdn.pixabay.com/photo/2022/05/18/12/04/flower-7205105_960_720.jpg" alt="cicek" />
+        <img src={img2} alt="img2" />
+    </div>
   )
 }
 
