5e938ade63f255212d9f36a26709aae1536b6eca
fatihh67-basarir <fatih.basarir67@gmail.com>
2022-08-01 13:48:00 +0300

    change 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
+import Footer from './Footer';
+import Content from './Content';
 
 function App() {
   return (
     <div className="App">
       <h1>APP</h1>
       <Header/>
+      <Content/>
+      <Footer/>
     </div>
   );
 }
